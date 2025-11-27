19507f2e6f9124ad368de3bbce9b4e5909c18ca1
Fawwaz Firdaus <fawwazfirdaus7@gmail.com>
2025-11-26 17:09:23 -0700

    Initial commit

---
+++ clavis/clavisApp.swift
@@ -0,0 +1,17 @@
+//
+//  clavisApp.swift
+//  clavis
+//
+//  Created by Fawwaz Firdaus on 11/26/25.
+//
+
+import SwiftUI
+
+@main
+struct clavisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
